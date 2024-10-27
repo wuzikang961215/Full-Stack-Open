9b66ce05c2cb4b44c2f1d0624b09e770b8a95ee0
Peter Wu <peterwu@Peters-Laptop.local>
2024-10-27 13:42:55 +1030

    part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,60 +1,104 @@
-const Header = (props) => {
-  console.log(props)
+import { useState } from 'react';
+
+// Header Component
+const Header = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+
+// Button Component
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+);
+
+// StatisticLine Component for displaying individual statistic lines
+const StatisticLine = ({ name, count }) => {
+  if (name === 'positive') {
+    return (
+      <div>{name} {count} %</div>
+    );
+  }
+  return (
+    <div>{name} {count}</div>
+  );
+};
+
+// Statistics Component for displaying all statistics
+const Statistics = ({ good, neutral, bad, total, average, percentPos }) => {
+  if (total === 0) {
+    return <div>No feedback given</div>
+  }
   return (
     <div>
-      <h1>{props.course}</h1>
+      <StatisticLine name='good' count={good} />
+      <StatisticLine name='neutral' count={neutral} />
+      <StatisticLine name='bad' count={bad} />
+      <StatisticLine name='total' count={total} />
+      <StatisticLine name='average' count={average} />
+      <StatisticLine name='positive' count={percentPos} />
     </div>
-  )
-}
+  );
+};
 
-const Part = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>{props.part} {props.exercises}</p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Part part = {props.part1} exercises = {props.exercises1}/>
-      <Part part = {props.part2} exercises = {props.exercises2}/>
-      <Part part = {props.part3} exercises = {props.exercises3}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Number of exercises {props.total}</p>
-    </div>
-  )
-}
-
+// App Component
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [score, setScore] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [percentPos, setPercentPos] = useState(0);
+
+  const goodClick = () => {
+    const updatedGood = good + 1;
+    const updatedTotal = updatedGood + neutral + bad;
+    const updatedScore = score + 1;
+    setGood(updatedGood);
+    setTotal(updatedTotal);
+    setScore(updatedScore);
+    setAverage(updatedScore / updatedTotal);
+    setPercentPos((updatedGood / updatedTotal) * 100);
+  };
+
+  const neutralClick = () => {
+    const updatedNeutral = neutral + 1;
+    const updatedTotal = updatedNeutral + good + bad;
+    setNeutral(updatedNeutral);
+    setTotal(updatedTotal);
+    setAverage(score / updatedTotal);
+    setPercentPos((good / updatedTotal) * 100);
+  };
+
+  const badClick = () => {
+    const updatedBad = bad + 1;
+    const updatedTotal = updatedBad + good + neutral;
+    const updatedScore = score - 1;
+    setBad(updatedBad);
+    setTotal(updatedTotal);
+    setScore(updatedScore);
+    setAverage(updatedScore / updatedTotal);
+    setPercentPos((good / updatedTotal) * 100);
+  };
 
   return (
     <div>
-      <Header course = {course} />
-      <Content 
-        part1 = {part1} part2 = {part2} part3 = {part3}
-        exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}
+      <Header title='give feedback' />
+      <Button handleClick={goodClick} text='good' />
+      <Button handleClick={neutralClick} text='neutral' />
+      <Button handleClick={badClick} text='bad' />
+      <Header title='statistics' />
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        total={total} 
+        average={average} 
+        percentPos={percentPos} 
       />
-      <Total total = {exercises1 + exercises2 + exercises3}/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
